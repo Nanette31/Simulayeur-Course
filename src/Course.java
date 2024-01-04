9489a96430cccff1944efef49a049d5c0a94380c
Hermine VAL D'ARGENT <herminegame@gmail.com>
2024-01-04 10:21:16 +0100

    entrée du nom de la course et calcul du nombre de chevaux de l'arrivée

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,37 @@
 
+import java.util.Random;
+import java.util.Scanner;
+
+// Course hippique de 12 à 20 cheveaux.
+// un tiercé s'intéresse au 3 premier du classement (18 chevaux);
+// le quarté au 4 premier du classement (16 chevaux);
+// Le quinté au 5 premier du classement (20 chevaux);
+
 public class Course {
- 
+
 	public static void main(String[] args) {
-			System.out.println("je fonctionnne");
-	}
+		Random rand = new Random();
+
+		// Liste des courses :
+		String [] tabNomCourses = {"Tiercé", "Quarté", "Quinté"};
+							
+		int nbChevauxArrivee = 0; // 
+
+		// Saisie le nom de la course :
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Bonjour veuillez saisir le nom de la course recherché :");
+		String nomCourse = scan.next();
+		for (int i = 0 ; i < tabNomCourses.length ; i++) {
+			if (tabNomCourses[i].equals(nomCourse)) {
+				nbChevauxArrivee = i + 3;
+				break;
+			}
+		}
+		System.out.println(nbChevauxArrivee);
+		//int noCheval = rand.nextInt(17) +1;
+	}				
 }
+
+
+
