b6841288c6e5d8047153d9fa8d7ab5ece7352b32
Nanette31 <herminegame@gmail.com>
2024-01-08 11:10:50 +0100

    modif du code

--- src/Course.java
+++ src/Course.java
@@ -10,38 +10,39 @@ public class Course {
 
 	public static void main(String[] args) {
 		int horses = 0;
-		int[] number = {};
-		boolean jeuxEnCours = true;
+		int[] number = new int [5];
+		boolean gamesInProgerss = true;
 		
-		while(jeuxEnCours == true ) {
+		while(gamesInProgerss == true ) {
 			System.out.println("Combien de cheveaux doit il y avoir dans la course ?");
 			Scanner scan = new Scanner(System.in);
 			horses = scan.nextInt();
 			
 			System.out.println("Quels type de courses voulez vous ?");
 			System.out.println("[1] Tiercé | [2] Quarté | [3] Qunité |"); 
-			Scanner scanchoix = new Scanner(System.in);
-			int choix = scanchoix.nextInt();
-			if  (choix >= 1 && choix <= 3) {
+			Scanner scanChoice = new Scanner(System.in);
+			int choice = scanChoice.nextInt();
+			if  (choice >= 1 && choice <= 3) {
 				int position =0;
-				while (position < choix + 2) {
+				while (position < choice + 2) {
 					Random rand = new Random();
-					int randomNum = rand.nextInt((horses - 0) + 1) + 0;
+					int randomNum = rand.nextInt(horses) + 1;
 					number[position] = randomNum;
 					position++;
 				}
-				System.out.println("La cours a les numeros : [ ");
+				System.out.print("La cours a les numeros : [ ");
 				for (int i = 0 ; i < number.length ; i++) {
-					System.out.println(number[i] + " ");
+					System.out.print(number[i] + " ");
 				}
 				System.out.println("]");
 			} else {
 				System.out.println("Mauvaise saisit");
 			}
 			System.out.println("Voulez vous continuer ? Si oui tapez la touche [O]");
-			Scanner continu = new Scanner(System.in);
-			if (continu.next().toLowerCase() != "o") {
-				jeuxEnCours = false;
+			Scanner continuous = new Scanner(System.in);
+			String continueOrNot = continuous.next();
+			if (!continueOrNot.equalsIgnoreCase("o")) {
+				gamesInProgerss = false;
 			}
 		}
 	}
